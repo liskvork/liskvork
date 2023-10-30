6c9bb7a831ec1042eee8c7bc8c94de7b4836e51e
Emily Flion <emneo@kreog.com>
2023-10-30 21:40:23 +0100

    Make headless not the default

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ static auto initArgs(int argc, const char **argv)
         .valueFromArgument("--headless")
         .valueFromEnvironmentVariable("LV_HEADLESS")
         .possibleValues(true, false)
-        .defaultValue(true)
+        .defaultValue(false)
         .implicit();
     // clang-format on
 
