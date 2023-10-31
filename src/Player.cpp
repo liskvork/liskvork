7bf3ea613a83ef3538e6a29867bc7920f3785fba
Alexandre Flion <huntears@kreog.com>
2023-10-31 15:52:06 +0100

    Add proper way to create and launch a player

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,4 +1,5 @@
 #include <filesystem>
+#include <iostream>
 #include <optional>
 #include <sys/resource.h>
 #include <unistd.h>
@@ -8,49 +9,42 @@
 
 namespace lv {
 
-std::optional<Player>
-Player::loadFromBinary(const std::filesystem::path &path, unsigned long memoryLimit, size_t timeLimit)
+Player::Player(const std::filesystem::path &path, unsigned long memoryLimit, size_t timeLimit):
+    _playerPID(-1),
+    _memoryLimit(memoryLimit),
+    _timeLimit(timeLimit),
+    _stdin(nullptr),
+    _stdout(nullptr)
 {
-    std::optional<Player> player = std::nullopt;
-
     if (!std::filesystem::exists(path) || !std::filesystem::is_regular_file(path)) {
         LFATAL("Cannot open player executable with path: {}", path.string());
-        return std::nullopt;
     }
-
-    // TODO(huntears): https://stackoverflow.com/a/5784634 -> Launch program with mem limit
-    const int mainProcessID = getpid();
-    const int newPID = fork();
-    if (newPID == 0) {
-        // We are in the new process here
-
+    _playerPID = fork();
+    if (_playerPID == -1) {
+        throw std::runtime_error("Failed to start child process");
+    }
+    if (_playerPID == 0) { // In child process
         // Set the memory limit for the new player
         const struct rlimit limits = {memoryLimit, memoryLimit};
         setrlimit(RLIMIT_DATA, &limits);
 
+        ::dup2(_write_pipe.read_fd(), STDIN_FILENO);
+        ::dup2(_read_pipe.write_fd(), STDOUT_FILENO);
+        _write_pipe.close();
+        _read_pipe.close();
         const char *playerArgv[2] = {path.c_str(), nullptr};
-        execv(path.c_str(), (char *const *) playerArgv);
+        if (execv(path.c_str(), (char *const *) playerArgv) == -1) {
+            // Note: no point writing to stdout here, it has been redirected
+            std::cerr << "Error: Failed to launch program" << std::endl;
+            ::exit(1);
+        }
     }
-
-    return player;
-}
-
-Player::Player(
-    std::string &&name, std::optional<std::string> &&version, std::optional<std::string> &&author,
-    std::optional<std::string> &&country, std::optional<std::string> &&www, std::optional<std::string> &&email, int PID,
-    unsigned long memoryLimit, size_t timeLimit
-):
-    _name(std::move(name)),
-    _version(std::move(version)),
-    _author(std::move(author)),
-    _country(std::move(country)),
-    _www(std::move(www)),
-    _email(std::move(email)),
-    _playerPID(PID),
-    _memoryLimit(memoryLimit),
-    _timeLimit(timeLimit)
-{
+    ::close(_write_pipe.read_fd());
+    ::close(_read_pipe.write_fd());
+    _write_buf = std::make_unique<__gnu_cxx::stdio_filebuf<char>>(_write_pipe.write_fd(), std::ios::out);
+    _read_buf = std::make_unique<__gnu_cxx::stdio_filebuf<char>>(_read_pipe.read_fd(), std::ios::in);
+    _stdin.rdbuf(_write_buf.get());
+    _stdout.rdbuf(_read_buf.get());
     LDEBUG("Loaded player with name: {}", _name);
 }
-
 }
--- src/Player.hpp
+++ src/Player.hpp
@@ -1,23 +1,19 @@
 #ifndef D6FFAF17_E755_47B1_BCC1_772F8C11FADE
 #define D6FFAF17_E755_47B1_BCC1_772F8C11FADE
 
+#include <ext/stdio_filebuf.h> // NB: Specific to libstdc++
 #include <filesystem>
 #include <optional>
+
+#include "CPipe.hpp"
 
 namespace lv {
 
 class Player {
 public:
-    static std::optional<Player>
-    loadFromBinary(const std::filesystem::path &path, unsigned long memoryLimit, size_t timeLimit);
+    Player(const std::filesystem::path &path, unsigned long memoryLimit, size_t timeLimit);
 
 private:
-    Player(
-        std::string &&name, std::optional<std::string> &&version, std::optional<std::string> &&author,
-        std::optional<std::string> &&country, std::optional<std::string> &&www, std::optional<std::string> &&email,
-        int PID, unsigned long memoryLimit, size_t timeLimit
-    );
-
     std::string _name;
     std::optional<std::string> _version;
     std::optional<std::string> _author;
@@ -27,6 +23,13 @@ private:
     int _playerPID;
     unsigned long _memoryLimit;
     size_t _timeLimit;
+
+    CPipe _write_pipe;
+    CPipe _read_pipe;
+    std::unique_ptr<__gnu_cxx::stdio_filebuf<char>> _write_buf = nullptr;
+    std::unique_ptr<__gnu_cxx::stdio_filebuf<char>> _read_buf = nullptr;
+    std::ostream _stdin;
+    std::istream _stdout;
 };
 
 }
