d39287c58aad726ebcfe3187354011efb210db03
Alexandre Flion <huntears@kreog.com>
2023-11-05 21:29:06 +0100

    Add better player cleaning + Other things
    CLI commands support has been added.
    A start of player crash handler has been started.
    Now sending some info to the player at the start

--- src/liskvork.hpp
+++ src/liskvork.hpp
@@ -4,6 +4,8 @@
 #include "Player.hpp"
 #include "configuration/ConfigHandler.hpp"
 
+void sigchld_handler(int sig);
+
 int liskvork(const configuration::ConfigHandler &config, lv::Player &player1, lv::Player &player2);
 
 #endif /* A197BE1C_C6B9_4B18_A918_5B607480DACA */
