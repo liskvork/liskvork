6422bad7feb50ef4076d89d2df29eb1cc31b677a
emneo <emneo@kreog.com>
2024-09-08 01:32:31 +0200

    Update CI
    Signed-off-by: emneo <emneo@kreog.com>

--- build.zig
+++ build.zig
@@ -92,15 +92,12 @@ pub fn build(b: *std.Build) !void {
 
     const test_step = b.step("test", "Run unit tests");
 
-    for (targets) |test_target| {
-        const unit_tests = b.addTest(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = b.resolveTargetQuery(test_target),
-            .optimize = optimize,
-        });
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = std_target,
+        .optimize = optimize,
+    });
 
-        const run_unit_tests = b.addRunArtifact(unit_tests);
-        run_unit_tests.skip_foreign_checks = true;
-        test_step.dependOn(&run_unit_tests.step);
-    }
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&run_unit_tests.step);
 }
