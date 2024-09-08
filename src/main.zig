174337d50499779aa11ddd95ef408da8e666289e
emneo <emneo@kreog.com>
2024-09-08 21:43:25 +0200

    Setup logger
    Signed-off-by: emneo <emneo@kreog.com>

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,28 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const logz = @import("logz");
+
 const build_config = @import("build_config");
+
 const config = @import("config.zig");
 
 const semver = std.SemanticVersion.parse(build_config.version) catch @compileError("Given version is not valid semver");
 
-pub fn main() !void {
-    std.debug.print("Launching liskvork version {any}\n", .{semver});
+const is_debug_build = builtin.mode == std.builtin.OptimizeMode.Debug;
 
+pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit() != .ok) @panic("memory leaked");
     const allocator = gpa.allocator();
+
+    try logz.setup(allocator, .{
+        .level = if (is_debug_build) .Debug else .Info,
+        .output = .stdout,
+        .encoding = .logfmt,
+    });
+    defer logz.deinit();
+
+    std.debug.print("Launching liskvork version {any}\n", .{semver});
 
     const conf = try config.parse("config.ini", allocator);
     std.debug.print("Got conf: {any}\n", .{conf});
