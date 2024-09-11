df99ce0f23c6490626b0941694251fa23aea4914
emneo <emneo@kreog.com>
2024-09-11 23:06:26 +0200

    Add zon2nix gen script
    Not ultra useful for me, but it is for documentation's sake
    
    Signed-off-by: emneo <emneo@kreog.com>

---
+++ scripts/gen_zon_nix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# This script is used to generate the build.zig.zon.zig at the root of the repo
+# It will not generate a file that respects the nix formatter
+# TODO: Generate according to the nix formatter
+
+# Use it as follow:
+# ./scripts/gen_zon_nix.sh > build.zig.zon.nix
+
+nix run github:emneo-dev/zon2nix
