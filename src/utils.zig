cd36f07492e4dfa5f7b4f1c5de16d60c610a1034
emneo <emneo@kreog.com>
2024-09-14 21:23:02 +0200

    Simplify utils.zig
    Signed-off-by: emneo <emneo@kreog.com>

--- src/utils.zig
+++ src/utils.zig
@@ -12,12 +12,22 @@ pub const SliceError = error{
     NonWhitespaceInTrim,
 };
 
+pub fn all_respect(T: type, slice: []const T, predicate: fn (val: T) bool) bool {
+    for (slice) |i| {
+        if (!predicate(i))
+            return false;
+    }
+    return true;
+}
+
+pub inline fn is_all_whitespace(slice: []const u8) bool {
+    return all_respect(u8, slice, std.ascii.isWhitespace);
+}
+
 pub fn skip_n_whitespace(slice: []const u8, n: usize) ![]const u8 {
     if (slice.len < n)
         return SliceError.SliceTooSmall;
-    for (0..n, slice[0..n]) |_, a| {
-        if (!std.ascii.isWhitespace(a))
-            return SliceError.NonWhitespaceInTrim;
-    }
+    if (!is_all_whitespace(slice[0..n]))
+        return SliceError.NonWhitespaceInTrim;
     return slice[n..];
 }
