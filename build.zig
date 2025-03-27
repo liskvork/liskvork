9aa49e66a64f63df2c9de0008cce0c393c9cd72e
Emily Flion <emneo@kreog.com>
2025-03-27 14:59:49 +0100

    Add zig version check in build.zig
    Signed-off-by: Emily Flion <emneo@kreog.com>

--- build.zig
+++ build.zig
@@ -127,6 +127,10 @@ pub fn build(b: *std.Build) !void {
     const native_target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    comptime {
+        if (@import("builtin").zig_version.order(.{ .major = 0, .minor = 14, .patch = 0 }) == .lt) @compileError("liskvork requires zig version >= 0.14.0");
+    }
+
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
